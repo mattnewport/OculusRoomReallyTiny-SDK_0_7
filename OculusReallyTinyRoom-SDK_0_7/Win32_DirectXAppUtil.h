ad240d39cefc34f22f62528b3eece59de79ae290
Matt Newport <matt@mattnewport.com>
2015-10-04 12:19:54 -0700

    Checking in more cleanup.

--- OculusReallyTinyRoom-SDK_0_7/Win32_DirectXAppUtil.h
+++ OculusReallyTinyRoom-SDK_0_7/Win32_DirectXAppUtil.h
@@ -99,6 +99,10 @@ struct DirectX11 {
     IDXGISwapChainPtr SwapChain;
     ID3D11Texture2DPtr BackBuffer;
     ID3D11RenderTargetViewPtr BackBufferRT;
+    ID3D11VertexShaderPtr D3DVert;
+    ID3D11PixelShaderPtr D3DPix;
+    ID3D11InputLayoutPtr InputLayout;
+    ID3D11SamplerStatePtr SamplerState;
     // Fixed size buffer for shader constants, before copied into buffer
     std::uint8_t UniformData[1024];
     ID3D11BufferPtr UniformBufferGen;
@@ -249,6 +253,59 @@ struct DirectX11 {
         Device->CreateBlendState(&bm, &bs);
         Context->OMSetBlendState(bs, nullptr, 0xffffffff);
 
+        auto compileShader = [](const char* src, const char* target) {
+            ID3DBlobPtr blob;
+            D3DCompile(src, std::strlen(src), nullptr, nullptr, nullptr, "main", target, 0, 0,
+                &blob, nullptr);
+            return blob;
+        };
+
+        // Create vertex shader and input layout
+        auto defaultVertexShaderSrc = R"(float4x4 ProjView;
+                                         void main(in float4 pos : POSITION,
+                                                   in float4 col : COLOR0,
+                                                   in float2 tex : TEXCOORD0,
+                                                   out float4 oPos : SV_Position,
+                                                   out float4 oCol : COLOR0,
+                                                   out float2 oTex : TEXCOORD0) {
+                                             oPos = mul(ProjView, pos);
+                                             oTex = tex;
+                                             oCol = col;
+                                         })";
+        ID3DBlobPtr vsBlob = compileShader(defaultVertexShaderSrc, "vs_4_0");
+        Device->CreateVertexShader(vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(),
+            nullptr, &D3DVert);
+        D3D11_INPUT_ELEMENT_DESC defaultVertexDesc[] = {
+            { "Position", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+            { "Color", 0, DXGI_FORMAT_B8G8R8A8_UNORM, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+            { "TexCoord", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 16, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+        };
+        Device->CreateInputLayout(defaultVertexDesc, std::size(defaultVertexDesc),
+            vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(),
+            &InputLayout);
+
+        // Create pixel shader
+        auto defaultPixelShaderSrc = R"(Texture2D Texture : register(t0);
+                                        SamplerState Linear : register(s0);
+                                        float4 main(in float4 Position : SV_Position,
+                                                    in float4 Color: COLOR0,
+                                                    in float2  TexCoord : TEXCOORD0) : SV_Target {
+                                            float4 TexCol = Texture.Sample(Linear, TexCoord);
+                                            if (TexCol.a==0) clip(-1); // If alpha = 0, don't draw
+                                            return(Color * TexCol);
+                                        })";
+        auto psBlob = compileShader(defaultPixelShaderSrc, "ps_4_0");
+        Device->CreatePixelShader(psBlob->GetBufferPointer(), psBlob->GetBufferSize(),
+            nullptr, &D3DPix);
+
+        // Create sampler state
+        CD3D11_SAMPLER_DESC ss{ D3D11_DEFAULT };
+        ss.Filter = D3D11_FILTER_ANISOTROPIC;
+        ss.AddressU = ss.AddressV = ss.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+        ss.MaxAnisotropy = 8;
+        ss.MaxLOD = 15;
+        Device->CreateSamplerState(&ss, &SamplerState);
+
         return true;
     }
 
@@ -288,7 +345,7 @@ struct DirectX11 {
 };
 
 // global DX11 state
-static struct DirectX11 DIRECTX;
+static DirectX11 DIRECTX;
 
 enum class TextureFill {
     AUTO_WHITE,
@@ -299,7 +356,7 @@ enum class TextureFill {
     AUTO_GRADE_256
 };
 
-auto Texture(TextureFill texFill) {
+auto createTexture(TextureFill texFill) {
     const int SizeW{256}, SizeH{256}, MipLevels{8};
     CD3D11_TEXTURE2D_DESC dsDesc(DXGI_FORMAT_R8G8B8A8_UNORM, SizeW, SizeH, 1, MipLevels,
                                     D3D11_BIND_SHADER_RESOURCE | D3D11_BIND_RENDER_TARGET);
@@ -351,64 +408,6 @@ auto Texture(TextureFill texFill) {
     return TexSv;
 }
 
-struct Material {
-    ID3D11VertexShaderPtr D3DVert;
-    ID3D11PixelShaderPtr D3DPix;
-    ID3D11ShaderResourceViewPtr Tex;
-    ID3D11InputLayoutPtr InputLayout;
-    ID3D11SamplerStatePtr SamplerState;
-
-    Material(ID3D11ShaderResourceView* t) : Tex{t} {
-        D3D11_INPUT_ELEMENT_DESC defaultVertexDesc[] = {
-            {"Position", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
-            {"Color", 0, DXGI_FORMAT_B8G8R8A8_UNORM, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0},
-            {"TexCoord", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 16, D3D11_INPUT_PER_VERTEX_DATA, 0},
-        };
-
-        ID3DBlobPtr blobData;
-
-        // Create vertex shader
-        auto defaultVertexShaderSrc =
-            "float4x4 ProjView;"
-            "void main(in  float4 Position  : POSITION,    in  float4 Color : COLOR0, in  float2 "
-            "TexCoord  : TEXCOORD0,"
-            "          out float4 oPosition : SV_Position, out float4 oColor: COLOR0, out float2 "
-            "oTexCoord : TEXCOORD0)"
-            "{   oPosition = mul(ProjView, Position); oTexCoord = TexCoord; "
-            "    oColor = Color; }";
-        D3DCompile(defaultVertexShaderSrc, strlen(defaultVertexShaderSrc), nullptr, nullptr,
-                   nullptr, "main", "vs_4_0", 0, 0, &blobData, nullptr);
-        DIRECTX.Device->CreateVertexShader(blobData->GetBufferPointer(), blobData->GetBufferSize(),
-                                           nullptr, &D3DVert);
-
-        // Create input layout
-        DIRECTX.Device->CreateInputLayout(defaultVertexDesc, std::size(defaultVertexDesc),
-                                          blobData->GetBufferPointer(), blobData->GetBufferSize(),
-                                          &InputLayout);
-
-        // Create pixel shader
-        auto defaultPixelShaderSrc =
-            "Texture2D Texture   : register(t0); SamplerState Linear : register(s0); "
-            "float4 main(in float4 Position : SV_Position, in float4 Color: COLOR0, in float2 "
-            "TexCoord : TEXCOORD0) : SV_Target"
-            "{   float4 TexCol = Texture.Sample(Linear, TexCoord); "
-            "    if (TexCol.a==0) clip(-1); "  // If alpha = 0, don't draw
-            "    return(Color * TexCol); }";
-        D3DCompile(defaultPixelShaderSrc, strlen(defaultPixelShaderSrc), nullptr, nullptr, nullptr,
-                   "main", "ps_4_0", 0, 0, &blobData, nullptr);
-        DIRECTX.Device->CreatePixelShader(blobData->GetBufferPointer(), blobData->GetBufferSize(),
-                                          nullptr, &D3DPix);
-
-        // Create sampler state
-        CD3D11_SAMPLER_DESC ss{D3D11_DEFAULT};
-        ss.Filter = D3D11_FILTER_ANISOTROPIC;
-        ss.AddressU = ss.AddressV = ss.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
-        ss.MaxAnisotropy = 8;
-        ss.MaxLOD = 15;
-        DIRECTX.Device->CreateSamplerState(&ss, &SamplerState);
-    }
-};
-
 struct Vertex {
     XMFLOAT3 Pos;
     DWORD C;
@@ -419,7 +418,7 @@ struct TriangleSet {
     std::vector<Vertex> Vertices;
     std::vector<short> Indices;
 
-    void AddSolidColorBox(float x1, float y1, float z1, float x2, float y2, float z2, DWORD c) {
+    void AddBox(float x1, float y1, float z1, float x2, float y2, float z2, DWORD c) {
         auto AddQuad = [this](Vertex v0, Vertex v1, Vertex v2, Vertex v3) {
             auto AddTriangle = [this](const std::initializer_list<Vertex>& vs) {
                 for (const auto& v : vs) {
@@ -479,13 +478,13 @@ struct TriangleSet {
 struct Model {
     XMFLOAT3 Pos;
     XMFLOAT4 Rot;
-    std::unique_ptr<Material> Fill;
+    ID3D11ShaderResourceViewPtr Tex;
     ID3D11BufferPtr VertexBuffer;
     ID3D11BufferPtr IndexBuffer;
     std::size_t NumIndices;
 
-    Model(const TriangleSet& t, XMFLOAT3 argPos, XMFLOAT4 argRot, Material* argFill)
-        : Pos(argPos), Rot(argRot), Fill(argFill), NumIndices{size(t.Indices)} {
+    Model(const TriangleSet& t, XMFLOAT3 argPos, XMFLOAT4 argRot, ID3D11ShaderResourceView* tex)
+        : Pos(argPos), Rot(argRot), Tex(tex), NumIndices{size(t.Indices)} {
         CD3D11_BUFFER_DESC vbDesc(size(t.Vertices) * sizeof(t.Vertices.back()),
                                   D3D11_BIND_VERTEX_BUFFER);
         D3D11_SUBRESOURCE_DATA vbData{t.Vertices.data(), 0, 0};
@@ -508,20 +507,20 @@ struct Model {
         memcpy(map.pData, &DIRECTX.UniformData, std::size(DIRECTX.UniformData));
         DIRECTX.Context->Unmap(DIRECTX.UniformBufferGen, 0);
 
-        DIRECTX.Context->IASetInputLayout(Fill->InputLayout);
+        DIRECTX.Context->IASetInputLayout(DIRECTX.InputLayout);
         DIRECTX.Context->IASetIndexBuffer(IndexBuffer, DXGI_FORMAT_R16_UINT, 0);
         const auto vbs = {VertexBuffer.GetInterfacePtr()};
         DIRECTX.Context->IASetVertexBuffers(0, size(vbs), begin(vbs), std::begin({sizeof(Vertex)}),
                                             std::begin({UINT(0)}));
         DIRECTX.Context->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 
-        DIRECTX.Context->VSSetShader(Fill->D3DVert, nullptr, 0);
-        DIRECTX.Context->PSSetShader(Fill->D3DPix, nullptr, 0);
+        DIRECTX.Context->VSSetShader(DIRECTX.D3DVert, nullptr, 0);
+        DIRECTX.Context->PSSetShader(DIRECTX.D3DPix, nullptr, 0);
 
-        const auto samplerStates = {Fill->SamplerState.GetInterfacePtr()};
+        const auto samplerStates = { DIRECTX.SamplerState.GetInterfacePtr()};
         DIRECTX.Context->PSSetSamplers(0, size(samplerStates), begin(samplerStates));
 
-        const auto texSrvs = {Fill->Tex.GetInterfacePtr()};
+        const auto texSrvs = {Tex.GetInterfacePtr()};
         DIRECTX.Context->PSSetShaderResources(0, size(texSrvs), begin(texSrvs));
         DIRECTX.Context->DrawIndexed(NumIndices, 0, 0);
     }
@@ -538,75 +537,62 @@ struct Scene {
 
     Scene() {
         TriangleSet cube;
-        cube.AddSolidColorBox(0.5f, -0.5f, 0.5f, -0.5f, 0.5f, -0.5f, 0xff404040);
-        Add(new Model(cube, {0, 0, 0}, {0, 0, 0, 1},
-                      new Material(Texture(TextureFill::AUTO_CEILING))));
+        cube.AddBox(0.5f, -0.5f, 0.5f, -0.5f, 0.5f, -0.5f, 0xff404040);
+        Add(new Model(cube, {0, 0, 0}, {0, 0, 0, 1}, createTexture(TextureFill::AUTO_CEILING)));
 
         TriangleSet spareCube;
-        spareCube.AddSolidColorBox(0.1f, -0.1f, 0.1f, -0.1f, +0.1f, -0.1f, 0xffff0000);
+        spareCube.AddBox(0.1f, -0.1f, 0.1f, -0.1f, +0.1f, -0.1f, 0xffff0000);
         Add(new Model(spareCube, {0, -10, 0}, {0, 0, 0, 1},
-                      new Material(Texture(TextureFill::AUTO_CEILING))));
+                      createTexture(TextureFill::AUTO_CEILING)));
 
         TriangleSet walls;
-        walls.AddSolidColorBox(10.1f, 0.0f, 20.0f, 10.0f, 4.0f, -20.0f, 0xff808080);   // Left Wall
-        walls.AddSolidColorBox(10.0f, -0.1f, 20.1f, -10.0f, 4.0f, 20.0f, 0xff808080);  // Back Wall
-        walls.AddSolidColorBox(-10.0f, -0.1f, 20.0f, -10.1f, 4.0f, -20.0f,
-                               0xff808080);  // Right Wall
-        Add(new Model(walls, {0, 0, 0}, {0, 0, 0, 1},
-                      new Material(Texture(TextureFill::AUTO_WALL))));
+        walls.AddBox(10.1f, 0.0f, 20.0f, 10.0f, 4.0f, -20.0f, 0xff808080);     // Left Wall
+        walls.AddBox(10.0f, -0.1f, 20.1f, -10.0f, 4.0f, 20.0f, 0xff808080);    // Back Wall
+        walls.AddBox(-10.0f, -0.1f, 20.0f, -10.1f, 4.0f, -20.0f, 0xff808080);  // Right Wall
+        Add(new Model(walls, {0, 0, 0}, {0, 0, 0, 1}, createTexture(TextureFill::AUTO_WALL)));
 
         TriangleSet floors;
-        floors.AddSolidColorBox(10.0f, -0.1f, 20.0f, -10.0f, 0.0f, -20.1f,
-                                0xff808080);  // Main floor
-        floors.AddSolidColorBox(15.0f, -6.1f, -18.0f, -15.0f, -6.0f, -30.0f,
-                                0xff808080);  // Bottom floor
+        floors.AddBox(10.0f, -0.1f, 20.0f, -10.0f, 0.0f, -20.1f, 0xff808080);    // Main floor
+        floors.AddBox(15.0f, -6.1f, -18.0f, -15.0f, -6.0f, -30.0f, 0xff808080);  // Bottom floor
         Add(new Model(floors, {0, 0, 0}, {0, 0, 0, 1},
-                      new Material(Texture(TextureFill::AUTO_FLOOR))));  // Floors
+                      createTexture(TextureFill::AUTO_FLOOR)));  // Floors
 
         TriangleSet ceiling;
-        ceiling.AddSolidColorBox(10.0f, 4.0f, 20.0f, -10.0f, 4.1f, -20.1f, 0xff808080);
+        ceiling.AddBox(10.0f, 4.0f, 20.0f, -10.0f, 4.1f, -20.1f, 0xff808080);
         Add(new Model(ceiling, {0, 0, 0}, {0, 0, 0, 1},
-                      new Material(Texture(TextureFill::AUTO_CEILING))));  // Ceiling
+                      createTexture(TextureFill::AUTO_CEILING)));  // Ceiling
 
         TriangleSet furniture;
-        furniture.AddSolidColorBox(-9.5f, 0.75f, -3.0f, -10.1f, 2.5f, -3.1f,
-                                   0xff383838);  // Right side shelf// Verticals
-        furniture.AddSolidColorBox(-9.5f, 0.95f, -3.7f, -10.1f, 2.75f, -3.8f,
-                                   0xff383838);  // Right side shelf
-        furniture.AddSolidColorBox(-9.55f, 1.20f, -2.5f, -10.1f, 1.30f, -3.75f,
-                                   0xff383838);  // Right side shelf// Horizontals
-        furniture.AddSolidColorBox(-9.55f, 2.00f, -3.05f, -10.1f, 2.10f, -4.2f,
-                                   0xff383838);  // Right side shelf
-        furniture.AddSolidColorBox(-5.0f, 1.1f, -20.0f, -10.0f, 1.2f, -20.1f,
-                                   0xff383838);  // Right railing
-        furniture.AddSolidColorBox(10.0f, 1.1f, -20.0f, 5.0f, 1.2f, -20.1f,
-                                   0xff383838);  // Left railing
+        furniture.AddBox(-9.5f, 0.75f, -3.0f, -10.1f, 2.5f, -3.1f,
+                         0xff383838);  // Right side shelf// Verticals
+        furniture.AddBox(-9.5f, 0.95f, -3.7f, -10.1f, 2.75f, -3.8f,
+                         0xff383838);  // Right side shelf
+        furniture.AddBox(-9.55f, 1.20f, -2.5f, -10.1f, 1.30f, -3.75f,
+                         0xff383838);  // Right side shelf// Horizontals
+        furniture.AddBox(-9.55f, 2.00f, -3.05f, -10.1f, 2.10f, -4.2f,
+                         0xff383838);  // Right side shelf
+        furniture.AddBox(-5.0f, 1.1f, -20.0f, -10.0f, 1.2f, -20.1f, 0xff383838);  // Right railing
+        furniture.AddBox(10.0f, 1.1f, -20.0f, 5.0f, 1.2f, -20.1f, 0xff383838);    // Left railing
         for (float f = 5; f <= 9; f += 1)
-            furniture.AddSolidColorBox(-f, 0.0f, -20.0f, -f - 0.1f, 1.1f, -20.1f,
-                                       0xff505050);  // Left Bars
+            furniture.AddBox(-f, 0.0f, -20.0f, -f - 0.1f, 1.1f, -20.1f, 0xff505050);  // Left Bars
         for (float f = 5; f <= 9; f += 1)
-            furniture.AddSolidColorBox(f, 1.1f, -20.0f, f + 0.1f, 0.0f, -20.1f,
-                                       0xff505050);                                    // Right Bars
-        furniture.AddSolidColorBox(1.8f, 0.8f, -1.0f, 0.0f, 0.7f, 0.0f, 0xff505000);   // Table
-        furniture.AddSolidColorBox(1.8f, 0.0f, 0.0f, 1.7f, 0.7f, -0.1f, 0xff505000);   // Table Leg
-        furniture.AddSolidColorBox(1.8f, 0.7f, -1.0f, 1.7f, 0.0f, -0.9f, 0xff505000);  // Table Leg
-        furniture.AddSolidColorBox(0.0f, 0.0f, -1.0f, 0.1f, 0.7f, -0.9f, 0xff505000);  // Table Leg
-        furniture.AddSolidColorBox(0.0f, 0.7f, 0.0f, 0.1f, 0.0f, -0.1f, 0xff505000);   // Table Leg
-        furniture.AddSolidColorBox(1.4f, 0.5f, 1.1f, 0.8f, 0.55f, 0.5f, 0xff202050);   // Chair Set
-        furniture.AddSolidColorBox(1.401f, 0.0f, 1.101f, 1.339f, 1.0f, 1.039f,
-                                   0xff202050);  // Chair Leg 1
-        furniture.AddSolidColorBox(1.401f, 0.5f, 0.499f, 1.339f, 0.0f, 0.561f,
-                                   0xff202050);  // Chair Leg 2
-        furniture.AddSolidColorBox(0.799f, 0.0f, 0.499f, 0.861f, 0.5f, 0.561f,
-                                   0xff202050);  // Chair Leg 2
-        furniture.AddSolidColorBox(0.799f, 1.0f, 1.101f, 0.861f, 0.0f, 1.039f,
-                                   0xff202050);  // Chair Leg 2
-        furniture.AddSolidColorBox(1.4f, 0.97f, 1.05f, 0.8f, 0.92f, 1.10f,
-                                   0xff202050);  // Chair Back high bar
+            furniture.AddBox(f, 1.1f, -20.0f, f + 0.1f, 0.0f, -20.1f, 0xff505050);  // Right Bars
+        furniture.AddBox(1.8f, 0.8f, -1.0f, 0.0f, 0.7f, 0.0f, 0xff505000);          // Table
+        furniture.AddBox(1.8f, 0.0f, 0.0f, 1.7f, 0.7f, -0.1f, 0xff505000);          // Table Leg
+        furniture.AddBox(1.8f, 0.7f, -1.0f, 1.7f, 0.0f, -0.9f, 0xff505000);         // Table Leg
+        furniture.AddBox(0.0f, 0.0f, -1.0f, 0.1f, 0.7f, -0.9f, 0xff505000);         // Table Leg
+        furniture.AddBox(0.0f, 0.7f, 0.0f, 0.1f, 0.0f, -0.1f, 0xff505000);          // Table Leg
+        furniture.AddBox(1.4f, 0.5f, 1.1f, 0.8f, 0.55f, 0.5f, 0xff202050);          // Chair Set
+        furniture.AddBox(1.401f, 0.0f, 1.101f, 1.339f, 1.0f, 1.039f, 0xff202050);   // Chair Leg 1
+        furniture.AddBox(1.401f, 0.5f, 0.499f, 1.339f, 0.0f, 0.561f, 0xff202050);   // Chair Leg 2
+        furniture.AddBox(0.799f, 0.0f, 0.499f, 0.861f, 0.5f, 0.561f, 0xff202050);   // Chair Leg 2
+        furniture.AddBox(0.799f, 1.0f, 1.101f, 0.861f, 0.0f, 1.039f, 0xff202050);   // Chair Leg 2
+        furniture.AddBox(1.4f, 0.97f, 1.05f, 0.8f, 0.92f, 1.10f,
+                         0xff202050);  // Chair Back high bar
         for (float f = 3.0f; f <= 6.6f; f += 0.4f)
-            furniture.AddSolidColorBox(3, 0.0f, -f, 2.9f, 1.3f, -f - 0.1f, 0xff404040);  // Posts
+            furniture.AddBox(3, 0.0f, -f, 2.9f, 1.3f, -f - 0.1f, 0xff404040);  // Posts
         Add(new Model(furniture, {0, 0, 0}, {0, 0, 0, 1},
-                      new Material(Texture(TextureFill::AUTO_WHITE))));  // Fixtures & furniture
+                      createTexture(TextureFill::AUTO_WHITE)));  // Fixtures & furniture
     }
 };
 
